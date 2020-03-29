987d629559d33cb19dae5dd8410770e1ac39abce
Matthias Quintero <matthiasquintero@gmail.com>
2020-03-28 21:15:53 -0600

    Filtering in driver list

---
+++ src/app/services/filter-service/filter-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FilterService {
+
+  url: string = environment.filterUri;
+
+  constructor(private http: HttpClient) { }
+
+  getFilteredDrivers(filters: Array<string>, location: string, batchId: number) {
+    return this.http.post(this.url, {
+      "filterTypes": filters,
+      "location": location,
+      "batchId": `${batchId}`
+    });
+  }
+}
