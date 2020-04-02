639e22d35b07355e5db589cc36ec7ee02123f0d9
Keith Wheeler <nemimimonkey@gmail.com>
2020-04-02 13:36:41 -0500

    fixed first test. working on second

--- e2e/test/nav.spec.ts
+++ e2e/test/nav.spec.ts
@@ -1,12 +1,30 @@
-import { browser, by, element } from 'protractor';
+import { browser, logging, by, element } from 'protractor';
+import { AppPage } from '../src/app.po';
 
 describe('navigation tests', function() {
-    let profilebutton = element(by.id('driverbuttontext'));
+    let page: AppPage;
 
-    element(by.id('navbarDropdown')).click();
-    browser.pause();
+    beforeEach(() => {
+        page = new AppPage();
+      });
 
+    let profilebuttonname = element(by.id('driverbuttontext'));
+    
     it('has the users name at top right', function() {
-        expect(profilebutton.getText()).toEqual('Fabien Braunroth');
+        expect(profilebuttonname.getText()).toEqual('Wain Vian');
     });
+
+    it('user can click on profile button', function() {
+        page.navigateTo();
+        page.getNavProfileA().click();
+        expect(element(by.id('profile-container-title')).getText()).toBe('Contact Information');
+    });
+
+    afterEach(async () => {
+        // Assert that there are no errors emitted from the browser
+        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
+        expect(logs).not.toContain(jasmine.objectContaining({
+          level: logging.Level.SEVERE,
+        } as logging.Entry));
+      });
 });
