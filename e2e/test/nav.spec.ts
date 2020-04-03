51039ffbe770e325a7509ff1bab681b7390f6d7f
Keith Wheeler <nemimimonkey@gmail.com>
2020-04-03 17:00:20 -0500

    both tests working

--- e2e/test/nav.spec.ts
+++ e2e/test/nav.spec.ts
@@ -9,15 +9,17 @@ describe('navigation tests', function() {
       });
 
     let profilebuttonname = element(by.id('driverbuttontext'));
+    let driverbuttonclicker = element(by.id('navbarDropdown'));
+    let profilebuttonclicker = element(by.id('nav-profile-a'));
     
     it('has the users name at top right', function() {
         expect(profilebuttonname.getText()).toEqual('Wain Vian');
     });
 
     it('user can click on profile button', function() {
-        page.navigateTo();
-        page.getNavProfileA().click();
-        expect(element(by.id('profile-container-title')).getText()).toBe('Contact Information');
+        driverbuttonclicker.click();
+        profilebuttonclicker.click();
+        expect(element(by.id('profile-container-title')).getText()).toBe('Contact Information:');
     });
 
     afterEach(async () => {
