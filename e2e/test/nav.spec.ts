00b26ffa74c1be47ee7c732120009f4ad9ca55bd
Keith Wheeler <nemimimonkey@gmail.com>
2020-04-04 11:57:21 -0500

    Update nav.spec.ts

--- e2e/test/nav.spec.ts
+++ e2e/test/nav.spec.ts
@@ -2,12 +2,6 @@ import { browser, logging, by, element } from 'protractor';
 import { AppPage } from '../src/app.po';
 
 describe('navigation tests', function() {
-    let page: AppPage;
-
-    beforeEach(() => {
-        page = new AppPage();
-      });
-
     let profilebuttonname = element(by.id('driverbuttontext'));
     let driverbuttonclicker = element(by.id('navbarDropdown'));
     let profilebuttonclicker = element(by.id('nav-profile-a'));
@@ -21,12 +15,4 @@ describe('navigation tests', function() {
         profilebuttonclicker.click();
         expect(element(by.id('profile-container-title')).getText()).toBe('Contact Information:');
     });
-
-    afterEach(async () => {
-        // Assert that there are no errors emitted from the browser
-        const logs = await browser.manage().logs().get(logging.Type.BROWSER);
-        expect(logs).not.toContain(jasmine.objectContaining({
-          level: logging.Level.SEVERE,
-        } as logging.Entry));
-      });
 });
